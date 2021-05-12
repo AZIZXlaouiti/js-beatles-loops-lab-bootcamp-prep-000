4c376a05897c92369cfae87a52a478be51db89d0
daring-demo-aacc <azizrocke@gmail.com>
2021-05-12 17:42:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var  new_array = [] 
+  for(let i=0,word = "";i<musicians.length;i++){
+    
+    word = musicians[i]+" plays "+instruments[i];
+    new_array.push(word)
+  }
+  return new_array
+}
+const facts = [
+        "He was the last Beatle to learn to drive",
+        "He was never a vegetarian",
+        "He was a choir boy and boy scout",
+        "He hated the sound of his own voice"
+      ]
+function johnLennonFacts(facts){
+  let i = 0;
+     while (i<facts.length){
+       facts[i]+="!!!"
+       i +=1
+     }
+     return facts
+      }
+function iLoveTheBeatles(num) {
+  let new_array = [];
+  
+  do {
+      new_array.push('I Love The Beatles')
+      num++;
+  } while (num <15);
+
+
+return new_array;
+}
+
+    
